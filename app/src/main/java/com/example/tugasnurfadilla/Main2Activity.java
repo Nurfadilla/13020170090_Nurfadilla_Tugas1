60e251fe31f17eb6d9c45788068a5f78edad3b77
Nurfadilla <dhyllarahman.99@gmail.com>
2020-05-11 23:17:09 +0700

    Linear Layout tugas mobile programming

---
+++ app/src/main/java/com/example/tugasnurfadilla/Main2Activity.java
@@ -0,0 +1,26 @@
+package com.example.tugasnurfadilla;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
